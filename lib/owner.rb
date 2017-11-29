e3fd50555bd736426300b9b6806c3a624096c341
Rebecca Tong <rtong@buffalo.edu>
2017-11-29 23:57:38 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,8 +61,8 @@ class Owner
   def sell_pets
     @pets.each do |pet,animals|
       animal.each do|animal|
-      animal.mood = "nervous"
-      end 
+        animal.mood = "nervous"
+      end
     end
   end
 
