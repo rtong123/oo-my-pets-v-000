e1fa0819ce9a4ccffc307d75246515ef19cebbeb
Rebecca Tong <rtong@buffalo.edu>
2017-11-30 00:08:38 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,7 +68,7 @@ class Owner
   end
 
   def list_pets
-    return "I have #{@pets[:fishes].length} fish, #{@pets[:dog].length} dog(s), and #{@pets[:cat]} cat(s)."
+    return "I have #{@pets[:fishes].length} fish, #{@pets[:dog].length} dog(s), and #{@pets[:cat].length} cat(s)."
   end
 
 
